44846789fa246393761ff180429a76202564a38b
Yusuf DS <62705303+ibroyusufola@users.noreply.github.com>
2021-07-08 19:15:57 +0100

    Moving the trial.

---
+++ trial.dart
@@ -0,0 +1,31 @@
+void main() {
+  int a = 1, b = 2;
+
+  void add() {
+    int c = a + b;
+    print(c);
+  }
+
+  add();
+
+  void subtract() {
+    int d = a - b;
+    print(d);
+  }
+
+  subtract();
+
+  void divide() {
+    double e = a / b;
+    print(e);
+  }
+
+  divide();
+
+  void multiply() {
+    int f = a * b;
+    print(f);
+  }
+
+  multiply();
+}
